6cc798038b38cd9f26d872ff4f4022bdb5210a66
최대현 <eogus0911@gmail.com>
2024-12-02 22:20:08 +0900

    Docs: README, schedule.sql add
    Lv 0 README API 명세서 추가, ERD, schedule.sql 파일 추가

---
+++ schedule.sql
@@ -0,0 +1,26 @@
+CREATE TABLE `Schedule` (
+	`schedule_id`	BIGINT	NOT NULL	DEFAULT AUTO_INCREMENT,
+	`title`	VARCHER(100)	NOT NULL,
+	`password`	VARCHER(100)	NOT NULL,
+	`contents`	VARCHAR(200)	NOT NULL,
+	`create_date`	DATETIME	NOT NULL,
+	`edit_date`	DATETIME	NOT NULL,
+	`user_id`	BIGINT	NOT NULL	DEFAULT AUTO_INCREMENT,
+	`user_name`	VARCHER(50)	NOT NULL
+);
+
+CREATE TABLE `User` (
+	`user_id`	BIGINT	NOT NULL	DEFAULT AUTO_INCREMENT,
+	`user_name`	VARCHER(50)	NOT NULL,
+	`email`	VARCHAR(100)	NOT NULL,
+	`create_date`	DATETIME	NOT NULL
+);
+
+ALTER TABLE `Schedule` ADD CONSTRAINT `PK_SCHEDULE` PRIMARY KEY (
+	`schedule_id`
+);
+
+ALTER TABLE `User` ADD CONSTRAINT `PK_USER` PRIMARY KEY (
+	`user_id`,
+	`user_name`
+);
